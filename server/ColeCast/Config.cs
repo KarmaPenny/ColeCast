c85051debeb4c2b76974f5af92a173af8b655a80
Cole Robinette <robinette.31@gmail.com>
2018-01-11 15:21:22 -0500

    who knows

---
+++ server/ColeCast/Config.cs
@@ -0,0 +1,23 @@
+﻿using System.Configuration;
+
+namespace ColeCast
+{
+    public static class Config
+    {
+        public static int port
+        {
+            get
+            {
+                return int.Parse(ConfigurationManager.AppSettings["port"]);
+            }
+        }
+
+        public static string com
+        {
+            get
+            {
+                return ConfigurationManager.AppSettings["com"];
+            }
+        }
+    }
+}
--- server/ColeCast/Program.cs
+++ server/ColeCast/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace ColeCast
 {
@@ -10,7 +12,14 @@ namespace ColeCast
     {
         static void Main(string[] args)
         {
-            new Receiver(3129).Start();
+            try
+            {
+                new Receiver().Start();
+            }
+            catch (Exception e)
+            {
+                File.WriteAllText("c:\\error.txt", e.Message);
+            }
         }
     }
 }
